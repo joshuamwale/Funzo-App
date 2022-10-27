bfb3027f8ea2336e1d7ff82d7a9099201b1bc4a2
Mwenje <mwenje24@gmail.com>
2022-10-27 10:34:14 +0300

    created discussion detailed page

--- src/components/student/StudentDiscussion.js
+++ src/components/student/StudentDiscussion.js
@@ -7,14 +7,14 @@ function StudentDiscussion() {
       <div className='row'>
           <div className='col-md-2 main-sidebar'><SideBar /></div>
           <div className='col-md-10'>
-                <div className="discussion-header mt-4">
-                  <span className='discussion-title'>Ongoing Discussion</span>
-                  <button className='btn btn-sm discussion-button button-right'>Start Discussion</button>
+                <div className="summary-header mt-4">
+                  <span className='summary-title'>Ongoing Discussion</span>
+                  <button className='btn btn-sm summary-button button-right'>Start Discussion</button>
                 </div>
-                <div className='discussion-list'>
+                <div className='summary-list'>
                   <table class="table mt-4 table-striped table-hover table-sm ">
                     <thead>
-                      <tr className='discussion-button'>
+                      <tr className='summary-button'>
                         <th scope="col"></th>
                         <th scope="col">Title</th>
                         <th scope="col">Initiater</th>
@@ -26,37 +26,37 @@ function StudentDiscussion() {
                         <th scope="row">1</th>
                         <td>Undestanding react Hooks</td>
                         <td>Tonny Oseko</td>
-                        <td><button className='btn btn-sm discussion-button'>Join</button></td>
+                        <td><button className='btn btn-sm summary-button'>Join</button></td>
                       </tr>
                       <tr>
                         <th scope="row">2</th>
                         <td>Introduction to Ruby active record</td>
                         <td>Mike Sigh</td>
-                        <td><button className='btn btn-sm discussion-button'>Join</button></td>
+                        <td><button className='btn btn-sm summary-button'>Join</button></td>
                       </tr>
                       <tr>
                         <th scope="row">3</th>
                         <td>Form Validation in React</td>
                         <td>Tonny Oseko</td>
-                        <td><button className='btn btn-sm discussion-button'>Join</button></td>
+                        <td><button className='btn btn-sm summary-button'>Join</button></td>
                       </tr>
                       <tr>
                         <th scope="row">4</th>
                         <td>Writting clean code and code restructuriing</td>
                         <td>Jane Doe</td>
-                        <td><button className='btn btn-sm discussion-button'>Join</button></td>
+                        <td><button className='btn btn-sm summary-button'>Join</button></td>
                       </tr>
                       <tr>
                         <th scope="row">5</th>
                         <td>Error handling in ruby on rails</td>
                         <td>Gideon Snitch</td>
-                        <td><button className='btn btn-sm discussion-button'>Join</button></td>
+                        <td><button className='btn btn-sm summary-button'>Join</button></td>
                       </tr>
                       <tr>
                         <th scope="row">6</th>
                         <td>Market reception on most popular languages</td>
                         <td>Glenn Mwangi</td>
-                        <td><button className='btn btn-sm discussion-button'>Join</button></td>
+                        <td><button className='btn btn-sm summary-button'>Join</button></td>
                       </tr>
                     </tbody>
                   </table>
--- src/components/student/StudentJoinedDiscussion.js
+++ src/components/student/StudentJoinedDiscussion.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import SideBar from './SideBar'
+import * as FiIcons from 'react-icons/fi';
 
 function StudentJoinedDiscussion() {
   return (
@@ -7,9 +8,68 @@ function StudentJoinedDiscussion() {
       <div className='row'>
           <div className='col-md-2 main-sidebar'><SideBar /></div>
           <div className='col-md-10'>
-              <div className="discussion-header mt-4">
-                  <span className='discussion-title'>Ongoing Discussion</span>
-                  <button className='btn btn-sm discussion-button button-right'>Start Discussion</button>
+              <div className="summary-header mt-4 container">
+                  <span className='summary-title'>Discussion Title</span>
+                  <button className='btn btn-sm summary-button button-right'>Start Discussion</button>
+              </div>
+              <div className='discussion-comments mt-3 container'>
+                <span className='discussion-description'>
+                  Lorem Ipsum dolor sit amet, consectetur adipiscing, sed eiusmod tempor incidunt
+                  Ipsum dolor sit amet, consectetur adipiscing, sed eiusmod tempor incidunt
+                  Ipsum dolor sit amet, consectetur adipiscing, sed eiusmod tempor incidunt
+                </span>
+                <div className='discussion-student-comments border-1 border-secondary  mt-3'>
+                  <div className='chats mt-1'>
+                    <span className='student-name'>Student 1</span>
+                    <div>
+                      Ipsum dolor sit amet, consectetur adipiscing, sed eiusmod tempor incidunt
+                      Ipsum dolor sit amet, consectetur adipiscing, sed eiusmod tempor incidunt
+                    </div>
+                  </div>
+
+                  <div className='chats mt-1'>
+                    <span className='student-name'>Student 2</span>
+                    <div>
+                      Ipsum dolor sit amet, consectetur adipiscing, sed eiusmod tempor incidunt
+                      Ipsum dolor sit amet, consectetur adipiscing, sed eiusmod tempor incidunt
+                    </div>
+                  </div>
+
+                  <div className='chats mt-1'>
+                    <span className='student-name'>Student 3</span>
+                    <div>
+                      Ipsum dolor sit amet, consectetur adipiscing, sed eiusmod tempor incidunt
+                      Ipsum dolor sit amet, consectetur adipiscing, sed eiusmod tempor incidunt
+                    </div>
+                  </div>
+
+                  <div className='chats mt-1'>
+                    <span className='student-name'>Student 4</span>
+                    <div>
+                      Ipsum dolor sit amet, consectetur adipiscing, sed eiusmod tempor incidunt
+                      Ipsum dolor sit amet, consectetur adipiscing, sed eiusmod tempor incidunt
+                    </div>
+                  </div>
+                  <div className='chats mt-1'>
+                    <span className='student-name'>Student 2</span>
+                    <div>
+                      Ipsum dolor sit amet, consectetur adipiscing, sed eiusmod tempor incidunt
+                      Ipsum dolor sit amet, consectetur adipiscing, sed eiusmod tempor incidunt
+                    </div>
+                  </div>
+
+                  <div className='chats mt-1'>
+                    <span className='student-name'>Student 5</span>
+                    <div>
+                      Ipsum dolor sit amet, consectetur adipiscing, sed eiusmod tempor incidunt
+                      Ipsum dolor sit amet, consectetur adipiscing, sed eiusmod tempor incidunt
+                    </div>
+                  </div>
+                </div>
+                <form className='new-comment border-1 border-secondary'>
+                  <input className='h-100 px-4 comment-text' type='text' placeholder='Add comment.......' />
+                  <button className='btn send-button'><FiIcons.FiSend/></button>
+                </form>
               </div>
           </div>
       </div>
