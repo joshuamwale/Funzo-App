5c9f73a8da7dc09d4245306c2de522e7e52820de
Mwenje <mwenje24@gmail.com>
2022-11-03 07:08:15 +0300

    added form submit functionality to the student settings page

--- src/components/student/StudentSettings.js
+++ src/components/student/StudentSettings.js
@@ -1,8 +1,61 @@
 import SideBar from './SideBar'
-import React, { useEffect, useState} from 'react'
+import React, { useState, useEffect} from 'react'
 import './studentsettings.css'
 
 function StudentSettings() {
+
+  const user = localStorage.getItem("user");
+
+  const [studentuser, setStudentUser] = useState([]);
+  useEffect(() => {
+    fetch("/students")
+      .then((r) => r.json())
+      .then((response) => {
+        // console.log(response)
+        setStudentUser(response);
+      });
+  }, []);
+
+  const session_user = studentuser.filter((student_user) => {
+    return student_user.email === `${user}`
+  });
+  // console.log(session_user)
+
+  const [studentdetails, setStudentDetails] = useState({
+    student_name: "",
+    email: "",
+    phone:"",
+    image: "",
+    password: ""
+  })
+
+  const handleChange = (e) =>(
+    setStudentDetails({
+      ...studentdetails,
+      [e.target.name]: e.target.value,
+    })
+  )
+  
+  function handleSubmit(e){
+    e.preventDefault()
+    const formData ={
+      student_name: studentdetails.student_name,
+      email: studentdetails.email,
+      phone: studentdetails.phone,
+      image: studentdetails.image,
+      password: studentdetails.password
+    }
+    // console.log(formData)
+    fetch("/student/signup", {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+  }
+
+
 
       return (
           <div>
@@ -11,15 +64,42 @@ function StudentSettings() {
                 <SideBar />
               </div>
               <div className="col-md-10">
-              <form onSubmit={this.handleSubmit} className="student-settings">
-                  <h1>Edit Profile</h1>
-                  <label className='form-label'>Name :</label> <input className='form-control' type="text" value={this.state.Name} onChange={this.Namehandler} placeholder="Name..." /><br />
-                  <label className='form-label'>Email :</label> <input className='form-control' type="text" value={this.state.email} onChange={this.emailhandler} placeholder="Email..." /><br />
-                  <label className='form-label'>Phone:</label> <input className='form-control' type="text" value={this.state.phone} onChange={this.phonehandler} placeholder="Phone..." /><br />
-                  <label className='form-label'>Image :</label> <input className='form-control-file' type="file" value={this.state.image} onChange={this.imagehandler} placeholder="Image..." /><br />
-                  <label className='form-label'>Password :</label> <input className='form-control' type="password" value={this.state.password} onChange={this.passwordhandler} placeholder="Password..." /><br />
+              <form onSubmit={handleSubmit} className="student-settings">
+                  <h3 className='mt-4'>Edit Profile</h3>
+                  <label className='form-label'>Name :</label>
+                  <input className='form-control' 
+                    type="text" 
+                    name ="student_name"
+                    value={session_user.student_name}
+                    onChange={handleChange} 
+                    placeholder="Name..." /><br />
+                  <label className='form-label'>Email :</label> 
+                  <input className='form-control' type="text" 
+                    name='email'
+                    value={session_user.email} 
+                    onChange={handleChange} 
+                    placeholder="Email..." /><br />
+                  <label className='form-label'>Phone:</label> 
+                  <input className='form-control' type="text" 
+                    value={session_user.phone} 
+                    onChange={handleChange} 
+                    placeholder="Phone..." /><br />
+                  <label className='form-label'>Image :</label> 
+                  <input className='form-control' 
+                    type="text" 
+                    name='image'
+                    value={session_user.image} 
+                    onChange={handleChange} 
+                    placeholder="Image url..." /><br />
+                  <label className='form-label'>Password :</label> 
+                  <input className='form-control' 
+                    type="password" 
+                    name='password'
+                    value={session_user.password} 
+                    onChange={handleChange} 
+                    placeholder="Password..." /><br />
                   
-                  <input type="submit" value="Submit" />
+                  <input className='btn btn-secondary btn-sm float-right' type="submit" value="Submit" />
               </form>
               </div>
               </div>
